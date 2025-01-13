4a45496e168b250897c941ce4321a5534e78988e
SpacePistache <lukeholley@hotmail.com>
2025-01-13 09:46:55 +0100

    chat factorial fix

---
+++ debugging/factorial.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+import math
+
+if len(sys.argv) != 2:
+    print("Usage: ./script_name.py <non-negative integer>")
+    sys.exit(1)
+
+try:
+    n = int(sys.argv[1])
+    if n < 0:
+        print("Factorial is not defined for negative numbers.")
+        sys.exit(1)
+    print(f"Factorial of {n} is {math.factorial(n)}")
+except ValueError:
+    print("Please provide a valid integer.")
+    sys.exit(1)
